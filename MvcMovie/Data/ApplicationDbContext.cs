de4f26329eb38849c4355a8eff334ff12282f306
“voducnhat12” <voducnhat7@gmail.com>
2025-03-01 18:31:42 +0700

    update

---
+++ MvcMovie/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
--- MvcMovie/Models/Person.cs
+++ MvcMovie/Models/Person.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
 namespace MvcMovie.Models
 {
+    [Table("Persons")]
     public class Person
     {
+        [Key]
         public string PersonId { get; set;}
         public string FullName { get; set;}
         public string Address { get; set;}
